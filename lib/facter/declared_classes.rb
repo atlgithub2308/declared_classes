4e6a2d47ab18457096aa36c788389a42af653205
TeckLeong <teckleong@-NWXW43QXK2.local>
2024-05-23 14:23:16 +0800

    aaa

--- lib/facter/declared_classes.rb
+++ lib/facter/declared_classes.rb
@@ -1,18 +1,13 @@
- Facter.add(:declared_classes_l) do
-   confine :osfamily => ['Debian', 'RedHat']
-   #confine :operatingsystemmajrelease => '10'
-   setcode do
-     s = Facter::Core::Execution.exec(
-       'cat /opt/puppetlabs/puppet/cache/state/classes.txt '
-     )
-   end
- end
-
- Facter.add(:declared_classes_w) do
-  confine :osfamily => 'windows'
+Facter.add(:declared_classes_l) do
+  confine osfamily: ['Debian', 'RedHat']
   setcode do
-    s = Facter::Core::Execution.exec(
-      'powershell "cat C:\ProgramData\PuppetLabs\puppet\cache\state\classes.txt" '
-    )
+    Facter::Core::Execution.exec('cat /opt/puppetlabs/puppet/cache/state/classes.txt ')
+  end
+end
+
+Facter.add(:declared_classes_w) do
+  confine osfamily: 'windows'
+  setcode do
+    Facter::Core::Execution.exec('powershell "cat C:\ProgramData\PuppetLabs\puppet\cache\state\classes.txt" ')
   end
 end
