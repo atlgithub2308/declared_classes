d462aaa0cd60abef734a6d3986058944f8ec30fb
Hoo Sooyean (何書淵) <Sooyean.hoo@perforce.com>
2024-05-23 15:59:45 +0800

    Added in role and profiles
    Signed-off-by: Hoo Sooyean (何書淵) <Sooyean.hoo@perforce.com>

---
+++ lib/facter/profiles.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Facter.add(:profiles) do
+  # https://puppet.com/docs/puppet/latest/fact_overview.html
+  confine :declared_classes do |value|
+    ! "#{value}#{Facter.value[:declared_classes_w]}#{Facter.value[:declared_classes_l]}".empty?
+  end
+  setcode do
+    "#{Facter.value(:declared_classes)} #{Facter.value(:declared_classes_w)} #{Facter.value(:declared_classes_l)} ".split(%r{[\n ]+}).reject{|x| x.nil? || x.empty? }.select{|x| x.match?(%r{^profile::|::profile::})}
+  end
+end
